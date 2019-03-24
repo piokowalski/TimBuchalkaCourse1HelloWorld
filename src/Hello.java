88197189e662a6be4fc778f7050e0e756f2cac2c
piokowalski <piokowalski@onet.pl>
2019-03-24 11:06:15 +0100

    Fourth exercise done - Strings

--- src/Hello.java
+++ src/Hello.java
@@ -36,13 +36,37 @@ public class Hello {
         System.out.println("longTotal = " + longTotal);
 
 
-        //Section3.21 Char and Boolean
+        //Section3.21 Char and Boolean (unicode table website used to generate this copyright symbol)
         char myChar1 = '&';
         char myCharUnicode = '\u00A9';
         System.out.println(myCharUnicode);
 
         boolean myBoolean = false;
         boolean isMale = true;
+
+
+        //Section3.22 String data type
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + "\u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString value: " + lastString);
+
 
     }
 }
