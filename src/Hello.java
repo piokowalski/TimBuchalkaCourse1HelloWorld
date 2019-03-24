36079be300ab51f981dc3165c5df7639b88630af
piokowalski <piokowalski@onet.pl>
2019-03-24 10:25:59 +0100

    First exercise done!

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,15 @@ public class Hello {
 
         System.out.println("Hello, newbie!");
 
+        int myFirstNumber = (10+5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+
+        int myLastOne = 1000 -myTotal;
+
+        System.out.println(myLastOne);
+
     }
 }
