ad17616e6811a9ad8ca5753c7b87c4b13af37deb
piokowalski <piokowalski@onet.pl>
2019-03-24 10:45:31 +0100

    Second exercise done - primitive values

--- src/Hello.java
+++ src/Hello.java
@@ -14,5 +14,26 @@ public class Hello {
 
         System.out.println(myLastOne);
 
+
+
+
+        //Primitive data types
+
+        int myMinValue = -2147483648;
+        int myMaxValue = 2_147_483_647;
+
+        byte myByteValue = -128;
+        //max is 127 like MIDI interface ;)
+
+        short myShortValue = -32768;
+        //max is 32767
+
+        long myLongValue = -9_223_372_036_854_775_807L;
+        //long has a width of 64, int 32, short 16, byte 8
+
+        //casting into long
+        long longTotal = 50000L + 10L * (myShortValue + myByteValue + myMaxValue);
+        System.out.println("longTotal = " + longTotal);
+
     }
 }
