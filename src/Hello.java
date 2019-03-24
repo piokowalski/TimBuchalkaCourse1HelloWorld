18e6a1287eb9ec055536dedc615a08fe6daf8ee0
piokowalski <piokowalski@onet.pl>
2019-03-24 11:38:56 +0100

    Fifth lesson - operators in Java

--- src/Hello.java
+++ src/Hello.java
@@ -68,5 +68,41 @@ public class Hello {
         System.out.println("lastString value: " + lastString);
 
 
+        // Section 3.23 Operators in Java
+
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " +result);
+
+        int previousResult = result;
+
+        result = result -1;
+        System.out.println(previousResult + " -1 = " +result);
+
+        result = result *10;
+        System.out.println(previousResult + " * 10 = " +result);
+
+        result = result /5;
+        System.out.println(previousResult + " / 5 = " +result);
+
+        previousResult = result;
+        result = result %3;
+        System.out.println(previousResult + " % 3 = " +result);
+
+        result++;
+        System.out.println("result is now " +result);
+
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("It is not an alien!");
+
+        int topScore = 80;
+        if (topScore < 100)
+            System.out.println("You got the high score!");
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100)
+            System.out.println("Greater than second top score and less than 100");
+
+
     }
 }
